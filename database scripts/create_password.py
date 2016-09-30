0e9d84e3d8621f92b070c16b9b17ee86b7e9884b
iancwwong <wong.iancw@gmail.com>
2016-10-01 07:56:04 +1000

    Added sql folder, and script to create an admin

--- database scripts/create_password.py
+++ database scripts/create_password.py
@@ -2,10 +2,24 @@
 # Then generate a salt, and the salted password (using the SHA1 algorithm)
 
 import hashlib
+import os
+import base64
 # -------------------------
 def generatePassword():
+
+	# Obtain user details
 	inputUsername = raw_input("Username: ")
 	inputPassword = raw_input("Password: ")
+	
+	# Obtain a random salt
+	salt = base64.b64encode(os.urandom(33))
+	password = hashlib.sha1(salt + inputPassword).hexdigest()
+	
+	print "== Your details =="
+	print "* Username: %s" % inputUsername
+	print "* Input password: %s" % inputPassword
+	print "* Salt: %s" % salt
+	print "* Password: %s" % password
 	
 
 # -------------------------
