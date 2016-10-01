f09c8fef2860dc3b8f4c515569e6308a992888c7
clouds1 <digitalpaperbag@gmail.com>
2016-10-01 12:45:05 +1000

    Fixed admin navbar stuff

--- DLBPlus/WebContent/adminNavbar.jsp
+++ DLBPlus/WebContent/adminNavbar.jsp
@@ -5,19 +5,11 @@
 	   	<div class="nav-wrapper container"><a id="logo-container" href="?action=home" class="brand-logo"><img src="images/logo_white.png" alt="logo"></a>
    			<ul class="right hide-on-med-and-down"> 			
 				<c:choose>
-					<c:when test="${not empty user}">
-						<li><a href="search.jsp">Advanced Search</a></li>
-						<li><a href="userAccount.jsp">Account</a></li>
-						<li><a href="cart.jsp">Cart</a></li>
-						<li><a></a></li>
-						<li><a href="?action=logout">Logout</a>
+					<c:when test="${not empty currAdmin}">
+						<li><a href="admin?action=viewAllUsers">Manage Users</a></li>
+						<li><a href="admin?action=viewAllListings">Manage Publications</a></li>
+						<li><a href="admin?action=adminLogout">Logout</a>
 					</c:when>
-						
-					<c:otherwise>
-						<li><a href="/DLBPlus/admin?Action=Users">Manage Users</a></li>
-						<li><a href="/DLBPlus/admin?Action=Listings">Manage Publications</a></li>
-						<li><a href="login.jsp">Login</a></li>
-					</c:otherwise>
 				</c:choose>
 	     </ul>
 	   </div>
--- DLBPlus/src/edu/unsw/comp9321/AdminServlet.java
+++ DLBPlus/src/edu/unsw/comp9321/AdminServlet.java
@@ -38,37 +38,7 @@ public class AdminServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// Redirect to login page if no admin is currently logged in; index page otherwise
-		String redir = "";
-		Admin currAdmin = (Admin) request.getSession().getAttribute("currAdmin");
-		if (currAdmin == null) {
-			//redir = "adminLogin.html";
-			RequestDispatcher rd = request.getRequestDispatcher("adminLogin.jsp");
-			rd.forward(request, response);
-		} else {
-			
-			String Action = request.getParameter("Action");
-			if(Action == null) {
-				RequestDispatcher rd = request.getRequestDispatcher("adminIndex.jsp");
-				rd.forward(request, response);
-			} else if (Action == "Users") {
-				
-				
-				List<User> ListOfUsers = this.db.GetAllUsers();
-				request.setAttribute("ListOfUsers", ListOfUsers);
-				
-				
-				RequestDispatcher rd = request.getRequestDispatcher("adminUsers.jsp");
-				rd.forward(request, response);
-			} else if (Action == "Listings") {
-	
-				
-				
-				RequestDispatcher rd = request.getRequestDispatcher("adminUsers.jsp");
-				rd.forward(request, response);
-			}
-		}
+		doPost(request,response);
 	}
 	
 	/**
@@ -76,30 +46,56 @@ public class AdminServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// Determine redirection page as appropriate to user action
-		String req = request.getParameter("action");
-		String nextPage = "adminIndex.jsp";		// default
-		if (req.equals("viewAllUsers")) {
-			nextPage = "adminUsers.jsp";	
+		// Check for action string
+		String action = request.getParameter("action");
+		Admin currAdmin = (Admin) request.getSession().getAttribute("currAdmin");
+		
+		// Case when no admin, and no action: login
+		if (currAdmin == null && action == null) {
+			RequestDispatcher rd = request.getRequestDispatcher("adminLogin.jsp");
+			rd.forward(request, response);
+			return;
 		}
 		
-		// Admin needs to log in
-		else if (req.equals("adminLogin")) {
-			// Obtain user login parameters
+		// Case when no admin, action = login: verify
+		if (currAdmin == null && action.equals("adminLogin")) {
 			String inputUsername = request.getParameter("username");
 			String inputPwd = request.getParameter("password");
 			
 			// Verify admin
 			if (this.db.VerifyAdmin(inputUsername, inputPwd)) {
-				Admin currAdmin = this.db.GetAdmin(inputUsername);
-				request.getSession().setAttribute("currAdmin", currAdmin);
+				Admin newAdmin = this.db.GetAdmin(inputUsername);
+				request.getSession().setAttribute("currAdmin", newAdmin);
+				System.out.println("Admin succesfully logged in!");
 			} else {
 				// Admin could not be verified - redirect back to login page
-				nextPage = "adminLogin.jsp";
-				request.getSession().setAttribute("errorMsg", "Incorrect username or password.");
+				request.setAttribute("errorMsg", "Incorrect username or password.");
+				RequestDispatcher rd = request.getRequestDispatcher("adminLogin.jsp");
+				rd.forward(request, response);
+				return;
 			}
 		}
+		
+		// At this point, admin is logged in. Redirect based on action
+		String nextPage = "adminIndex.jsp";
+		if (action == null) {
+			// no action specified
+			nextPage = "adminIndex.jsp";
 
+		} else if (action.equals("viewAllUsers")) {
+			nextPage = "adminUsers.jsp";
+		} else if (action.equals("viewAllListings")) {
+			List<User> ListOfUsers = this.db.GetAllUsers();
+			request.setAttribute("ListOfUsers", ListOfUsers);
+			
+			nextPage = "adminPublications.jsp";
+		} else if (action.equals("adminLogout")) {
+			
+			System.out.println("Admin wants to log out...");
+			request.getSession().removeAttribute("currAdmin");
+			nextPage = "adminLogin.jsp";
+		} 
+		
 		RequestDispatcher rd = request.getRequestDispatcher(nextPage);
 		rd.forward(request, response);
 	}
